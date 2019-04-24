0acf2358abe60f6d9a915147bf03cc224ad10ad9
Asus <Asus@172.16.96.22>
2019-04-24 13:51:08 +0700

    Ini buat UTS

--- src/komputer.java
+++ src/komputer.java
@@ -3,19 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tugasuts;
 
 /**
  *
  * @author Asus
  */
-public class TugasUTS {
-
-    /**
-     * @param args the command line arguments
-     */
+public class komputer {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hallo dunia");
     }
-    
 }
