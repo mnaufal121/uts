f334f8b3ab0ad5db218147cd159b4546f42bd587
Asus <Asus@172.16.96.22>
2019-04-24 14:39:28 +0700

    update pada kelas cpu,dan kelas anak intel dan amd

--- src/tugasuts/cpu.java
+++ src/tugasuts/cpu.java
@@ -10,5 +10,30 @@ package tugasuts;
  * @author Asus
  */
 public class cpu {
+    protected String nama;
+    protected int kecepatan;
     
+    public cpu(String nama, int kecepatan){
+        this.nama = nama;
+        this.kecepatan = kecepatan;
+    }
+    public cpu(){
+        
+    }
+    public String getCPUData(){
+        return nama+" "+kecepatan;
+    }
+}
+class intel extends cpu{    
+    public intel(int kecepatan) {
+      this.kecepatan = kecepatan;
+      this.nama="Intel";
+    }
+
+class amd extends cpu{
+    public amd(int kecepatan){
+        this.kecepatan = kecepatan;
+        this.nama = "AMD";
+    }
+}
 }
