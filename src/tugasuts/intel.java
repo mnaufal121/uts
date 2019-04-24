16a43c95af5b98d790f020be5f6567f1fcae4e79
Asus <Asus@172.16.96.22>
2019-04-24 13:59:27 +0700

    Ini buat UTS

--- src/tugasuts/intel.java
+++ src/tugasuts/intel.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package tugasuts;
 
 /**
  *
  * @author Asus
  */
-public class komputer {
-    public static void main(String[] args) {
-        System.out.println("hallo dunia");
-    }
+public class intel {
+    
 }
